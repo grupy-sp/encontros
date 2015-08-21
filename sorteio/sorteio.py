699fec8e6b4282c6bacc05e02b985c7a9bb3b529
erichideki <hidekinhow@hotmail.com>
2015-08-21 13:56:35 -0300

    Script run on python2 and python 3 - Murilo Viana helped

--- sorteio/sorteio.py
+++ sorteio/sorteio.py
@@ -1,5 +1,6 @@
 import random #biblioteca python para sorteios
 import webbrowser #biblioteca python para abrir navegadores
+import sys
 
 convidados = [
 "http://www.meetup.com/pt/Grupy-SP/members/189311916/",
@@ -128,7 +129,10 @@ convidados = [
 
 while True:
     print('S para sorteio, P para parar')
-    opt = input()
+    if sys.version_info.major == 3:
+        opt = input()
+    else:
+        opt = raw_input()
     if opt in ['S', 's']:
         ganhador = random.sample(convidados, 1)
         convidados.remove(ganhador[0])
